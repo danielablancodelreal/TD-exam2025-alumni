5bd7d767042b122ea2b0670de9ce2b101171b14a
Daniela Blanco del Real <danielablancodelreal@alu.icai.comillas.edu>
2025-04-28 11:51:39 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 resource "google_container_cluster" "primary" {
-  name     = 
-  location = 
+  name     = ${var.gcp-cluster-name}
+  location = ${var.gcp-zone}
 
   remove_default_node_pool = true
   initial_node_count       = 1
@@ -13,8 +13,8 @@ resource "google_container_cluster" "primary" {
 resource "google_container_node_pool" "primary_nodes" {
   name       = "${var.gcp-cluster-name}-node-pool"
   cluster    = google_container_cluster.primary.name
-  location   = 
-  node_count = 
+  location   = ${var.gcp-zone}
+  node_count = ${var.gcp-node-count}
 
   node_config {
     machine_type = 
@@ -26,8 +26,8 @@ resource "google_container_node_pool" "primary_nodes" {
 }
 
 resource "google_storage_bucket" "td_bucket" {
-  name     = 
-  location = 
+  name     = ${var.gcp-bucket-name}
+  location = ${var.gcp-zone}
   force_destroy = true
 
   uniform_bucket_level_access = true
